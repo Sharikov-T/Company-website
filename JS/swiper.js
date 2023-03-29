5703bcf3435182968e60997b69e8f06d0d790eed
Тимофей Шариков <s.tim39@mail.ru>
2023-03-29 17:00:57 +0300

    Адаптация раздела "О нас"

--- JS/swiper.js
+++ JS/swiper.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
-    spaceBetween: 50,
+    spaceBetween: 20,
     loop: true,
     navigation: {
         nextEl: ".swiper-button-next",
@@ -8,6 +8,21 @@ const swiper = new Swiper(".mySwiper", {
     },
     pagination: {
         el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+    observer: true,
+    observeParents: true,
+});
+
+const swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination-mobile",
         clickable: true,
         dynamicBullets: true,
     },
