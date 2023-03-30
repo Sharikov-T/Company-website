c85942121c95be110bd63859dd6f9d7a379484f7
Тимофей Шариков <s.tim39@mail.ru>
2023-03-30 14:12:29 +0300

    Реализована функция закрытия бургер-меню при клике на ссылку

--- JS/optimize.js
+++ JS/optimize.js
@@ -1,10 +1,16 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
-menuBtn.addEventListener('click', function(){
+menuBtn.addEventListener('click', function () {
 	menu.classList.add('active');
 })
 
-let menuClose = document.querySelector('.menu-close')
+let menuClose = document.querySelector('.menu-close');
+let menuCloseLink = document.querySelectorAll('.menu-link');
+for (let i = 0; i < menuCloseLink.length; ++i) {
+	menuCloseLink[i].addEventListener('click', function name() {
+		menu.classList.remove('active');
+	})
+}
 menuClose.addEventListener('click', function name(params) {
 	menu.classList.remove('active');
 })
